153c5c486b8030744666cde45c67447b42ca69c3
sinitsynme <72615475+sinitsynme@users.noreply.github.com>
2020-11-18 15:41:53 +0400

    Create Program.cs
    Initial commit

---
+++ Program.cs
@@ -0,0 +1,197 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+        //создал словарь, подробнее в методе CreateCards
+        private static Dictionary<int, string> _cards = new Dictionary<int, string>(); 
+        
+        private static int count = 0; //считает подходящие сочетания
+        
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8; //даёт вывести символы крестушек/пикушек...
+            CreateCards(ref _cards);
+            int[] fiveCards = new int[5]; //для ключей карт, вошедших в сочетание
+
+            Rec(0,0,ref fiveCards); //вызывает перебор всех сочетаний
+            Console.WriteLine();
+            Console.WriteLine("Число сочетаний равно " + count);
+            Console.ReadLine();
+        }
+
+
+        static void Rec(int idx, int last, ref int[] fiveCards)
+        {
+            if (idx == 5)
+            {
+                if (Test(fiveCards))
+                {
+                    count++;
+                    Out(fiveCards, _cards);
+                }
+                return;
+            }
+
+            for (int i = last + 1; i <= 36;i++)
+            {
+                fiveCards[idx] = i;
+                Rec(idx+1, i, ref fiveCards);
+                
+            }
+            
+        } //рекурсивный перебор - не трогаем!!!
+        
+
+        static void CreateCards(ref Dictionary<int, string> cards)
+        {
+            /*
+                Ребятки, этот метод не трогаем, он нам генерирует библиотеку всех существующих
+                36 карт. 
+                Библиотека - структура данных, которая хранит в себе данные в виде
+                таких пар, как "ключ-значение". По ключу из словаря мы можем доставать значения, обращаемся как к массиву
+                К примеру, вызвав команду cards[1] мы получим 6♥, cards[2] нам выдаст 6♦ и так далее.
+                
+                ---СПИСОК КЛЮЧЕЙ И КАРТ---
+                
+                key - value
+                1 - 6♥
+                2 - 6♦
+                3 - 6♣
+                4 - 6♠
+                5 - 7♥
+                6 - 7♦    (cards[6] выведет 7♦)...
+                7 - 7♣
+                8 - 7♠
+                9 - 8♥
+                10 - 8♦
+                11 - 8♣
+                12 - 8♠
+                13 - 9♥
+                14 - 9♦
+                15 - 9♣
+                16 - 9♠
+                17 - 10♥
+                18 - 10♦
+                19 - 10♣
+                20 - 10♠    (cards[20] выведет 10♠)...
+                21 - J♥
+                22 - J♦
+                23 - J♣
+                24 - J♠
+                25 - Q♥ 
+                26 - Q♦ 
+                27 - Q♣ 
+                28 - Q♠ 
+                29 - K♥
+                30 - K♦
+                31 - K♣
+                32 - K♠
+                33 - A♥ 
+                34 - A♦ 
+                35 - A♣ 
+                36 - A♠ 
+                
+                !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                определить масть по ключу:
+                ♥ -> ключ % 4 = 1
+                ♦ -> ключ % 4 = 2
+                ♣ -> ключ % 4 = 3
+                ♠ -> ключ % 4 = 0
+                определить номер/букву карты по ключу можно через неравенства (для 6 это (key>=1 && key <=4)
+                
+            */
+            int count = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                string s;
+                if (i > 0 && i < 6)
+                {
+                    s = (i + 5).ToString();
+                }
+
+                else if (i == 6)
+                {
+                    s = "J";
+                }
+
+                else if (i == 7)
+                {
+                    s = "Q";
+                }
+
+                else if (i == 8)
+                {
+                    s = "K";
+                }
+                else
+                {
+                    s = "A";
+                }
+                
+                cards.Add(count++, s+"♥");
+                cards.Add(count++, s+"♦");
+                cards.Add(count++, s+"♣");
+                cards.Add(count++, s+"♠");
+                
+            }
+        }
+
+        static void Out(int[] s, Dictionary<int, string> cards) //выводит карты
+        {
+            foreach (int key in s)
+            {
+                Console.Write(cards[key] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+        /*ПРИМЕР АЛГОРИТМА
+         12 вар -  Нужно, чтобы ТОЧНО были 2 дамы, 2 туза, 1 карта пиковой масти
+         то есть надо проверить массив ключей на то, что у нас есть 2 непиковых дамы, 2 непиковых туза 
+         и 1 пикушка
+        */
+        static bool Test(int[] fiveCards) //Редактируем только Test под свой вариант
+        {
+            int queens = 0;
+            int spades = 0; //создаю три интовых переменных, чтобы можно было отслеживать
+            int aces = 0; // число дам, тузов, пикушек  
+            foreach (int key in fiveCards)
+            {
+                if (key>=25 && key <= 27) //у непиковых дам ключи 25, 26, 27
+                {
+                    queens++;
+                }
+
+                if (key % 4 == 0 && key != 28 && key != 36) //определяю, ключ пиковый или нет + чтобы не был тузом или дамой
+                {
+                    spades++;
+                }
+
+                if (key >= 33 && key <= 35) // у непиковых тузов ключи 33, 34, 35
+                {
+                    aces++;
+                }
+
+                if (key == 36) //отдельно отслеживаем пикового туза...
+                {
+                    spades++;
+                    aces++;
+                }
+
+                if (key == 28)//...и пиковую даму
+                {
+                    spades++;
+                    queens++;
+                }
+            }
+
+            return (queens == 2 && spades == 1 && aces == 2);
+        }
+
+    }
+}
